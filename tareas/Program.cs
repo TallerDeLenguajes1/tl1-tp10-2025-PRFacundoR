552a58bd5f4a05d24681046d2783d244604bec13
FacundoPeraltaRomero <facundoperalta217@gmail.com>
2025-06-17 15:54:21 -0300

    punto 1 hecho

---
+++ tareas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using tareas;
+
+// See https://aka.ms/new-console-template for more information
+
+//
+//
+//
+
+Console.WriteLine("Ingres un url");
+string url = Console.ReadLine();
+
+HttpClient peticion = new HttpClient(); //traigo informacion de la web
+           
+HttpResponseMessage respuesta = await peticion.GetAsync(url);
+           
+respuesta.EnsureSuccessStatusCode();
+
+
+string responseBody = await respuesta.Content.ReadAsStringAsync();
+List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+foreach (var listT in listTareas)
+{
+    if (listT.completed == true)
+    {
+        Console.WriteLine("Titulo de la tarea: " + listT.title);
+        Console.WriteLine("Estado de la tarea, completado");
+
+    }
+
+
+   
+
+}
+
+
+
+foreach (var listT in listTareas)
+{
+    if (listT.completed == false)
+    {
+        Console.WriteLine("Titulo de la tarea: " + listT.title);
+        Console.WriteLine("Estado de la tarea, pendiente");
+
+    }
+
+
+}
+string jsonString = JsonSerializer.Serialize(listTareas);
+
+string ruta = "tareas.json";
+
+File.AppendAllText(ruta, jsonString + Environment.NewLine);
+
+
+
+
---
+++ tareas/Tarea.cs
@@ -0,0 +1,25 @@
+//clases estaticas no se instancias, osea no hacer falta newsd
+
+
+namespace tareas
+{
+    public class Tarea
+    {
+
+        public int userId { get; set; }
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public bool completed { get; set; }
+
+        
+        //void mostrar()
+        // deserealizar()
+        //serealizar()
+    }
+    
+
+
+    
+}
